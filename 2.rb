20cff133b58b5fb57716d74af508aea7afe3fc0c
appodols <alexander_podolsky@alumni.brown.edu>
2018-06-13 16:39:17 -0400

    problem #2

---
+++ 2.rb
@@ -0,0 +1,39 @@
+#Time complexity is O(n) where n is the length of the string
+#Space Complexity is O(n)
+
+def decode_string(s)
+  number_stack = []
+  str_stack = []
+  numbers = {}
+  (0..10).to_a.each{|num| numbers[num.to_s] = true}
+  i = 0
+  number = ""
+  while i < s.length
+    if numbers[s[i]]
+      number += s[i]
+      if numbers[s[i+1]]
+      else
+        number_stack.push(number.to_i)
+        number = ""
+      end
+     elsif s[i] == "]"
+      to_add = ""
+      until str_stack[-1] == "["
+       to_add += str_stack.pop
+      end
+      str_stack.pop
+      str_stack.push(to_add * number_stack.pop)
+    else
+      str_stack.push(s[i])
+    end
+    i += 1
+  end
+  result = ""
+  str_stack.each do |word|
+    result += word.reverse
+  end
+  result
+end
+
+
+#we can also implement a recursive
